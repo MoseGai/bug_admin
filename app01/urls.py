f5dec2b1065e1c376627460077ff0b6407ecbd64
MosGai <1055734536@qq.com>
2020-04-14 20:29:54 +0800

    登录页面的实现和逻辑业务的改进

--- app01/urls.py
+++ app01/urls.py
@@ -1,11 +1,10 @@
-#!/urs/bin/env python
-# _*_ coding:utf-8 _*_
-from django.conf.urls import url,include
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from django.conf.urls import url, include
 from django.contrib import admin
 from app01 import views
 
-urlpatterents = [
-    url(r'send/sms/',views.send_sms),
-    url(r'^register/',views.register,name='register'),# "app01:register"
-    
+urlpatterns = [
+    url(r'^send/sms/', views.send_sms),
+    url(r'^register/', views.register,name='register'), # "app01:register"
 ]
--- utils/tencent/sms.py
+++ utils/tencent/sms.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-import ssl
 
-# ssl._create_default_https_context = ssl._create_unverified_context
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 from qcloudsms_py import SmsMultiSender, SmsSingleSender
 from qcloudsms_py.httpclient import HTTPError
+
+from django.conf import settings
 
 
 def send_sms_single(phone_num, template_id, template_param_list):
@@ -16,10 +18,9 @@ def send_sms_single(phone_num, template_id, template_param_list):
     :param template_param_list: 短信模板所需参数列表，例如:【验证码：{1}，描述：{2}】，则传递参数 [888,666]按顺序去格式化模板
     :return:
     """
-    appid = 112142311  # 自己应用ID
-    appkey = "8cc5b87123y423423412387930004"  # 自己应用Key
-    sms_sign = "Python之路"  # 自己腾讯云创建签名时填写的签名内容（使用公众号的话这个值一般是公众号全称或简称）
-
+    appid = settings.TENCENT_SMS_APP_ID
+    appkey = settings.TENCENT_SMS_APP_KEY
+    sms_sign = settings.TENCENT_SMS_SIGN
     sender = SmsSingleSender(appid, appkey)
     try:
         response = sender.send_with_param(86, phone_num, template_id, template_param_list, sign=sms_sign)
@@ -36,9 +37,9 @@ def send_sms_multi(phone_num_list, template_id, param_list):
     :param param_list:短信模板所需参数列表，例如:【验证码：{1}，描述：{2}】，则传递参数 [888,666]按顺序去格式化模板
     :return:
     """
-    appid = 112142311
-    appkey = "8cc5b87123y423423412387930004"
-    sms_sign = "Python之路"
+    appid = settings.TENCENT_SMS_APP_ID
+    appkey = settings.TENCENT_SMS_APP_KEY
+    sms_sign = settings.TENCENT_SMS_SIGN
 
     sender = SmsMultiSender(appid, appkey)
     try:
@@ -46,11 +47,3 @@ def send_sms_multi(phone_num_list, template_id, param_list):
     except HTTPError as e:
         response = {'result': 1000, 'errmsg': "网络异常发送失败"}
     return response
-
-
-if __name__ == '__main__':
-    result1 = send_sms_single("15131255089", 548760, [666, ])
-    print(result1)
-
-    result2 = send_sms_single( ["15131255089", "15131255089", "15131255089", ],548760, [999, ])
-    print(result2)
